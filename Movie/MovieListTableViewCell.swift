387810ed086609b71edbf09b2a33d915b162e44c
Navneet+Cavan <navneet.s@emirates.com>
2018-04-25 12:03:21 +0400

    Added Movie List VC & Cell

---
+++ Movie/MovieListTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieListTableViewCell.swift
+//  Movie
+//
+//  Created by Navneet on 4/25/18.
+//  Copyright © 2018 Navneet. All rights reserved.
+//
+
+import UIKit
+
+class MovieListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterImgView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

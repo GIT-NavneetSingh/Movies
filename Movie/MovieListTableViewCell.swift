8c49ff15a7977b27d8c77bda545aad2d676d1a00
Navneet <navneet.s@emirates.com>
2018-04-28 16:21:43 +0400

    Added List View

--- Movie/MovieListTableViewCell.swift
+++ Movie/MovieListTableViewCell.swift
@@ -8,22 +8,70 @@
 
 import UIKit
 
+public typealias DownloadBlocks = (_ data: NSData?) -> ()
+
+struct CellViewModel {
+    
+    let movie: Movie
+    let cache: NSCache<AnyObject, AnyObject>
+
+    func fetchImage(from path: String?, completion: @escaping DownloadBlock) {
+        guard
+            let path = path,
+            let url = URL(string: "http://image.tmdb.org/t/p/w92\(path)") else {
+                completion(nil)
+                return
+        }
+
+        let engine = NetworkEngine()
+        engine.download(url, completion: completion)
+    }
+}
+
+
 class MovieListTableViewCell: UITableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var releaseDateLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
     @IBOutlet weak var posterImgView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    var viewModel: CellViewModel?
+    
+    func configureView() {
+        titleLabel.text = viewModel?.movie.title
+        releaseDateLabel.text = viewModel?.movie.releaseDate
+        overviewLabel.text = viewModel?.movie.overview
+        
+        setImage()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    fileprivate func setImage() {
+        guard let path = viewModel?.movie.posterPath else { return }
+        
+        if let cachedData = viewModel?.cache.object(forKey: path as AnyObject) as? Data {
+            activityIndicator.isHidden = true
+            posterImgView.image = UIImage(data: cachedData)
+        } else {
+            activityIndicator.isHidden = false
+            activityIndicator.startAnimating()
+            viewModel?.fetchImage(from: path, completion: { [weak self] data in
+                DispatchQueue.main.async {
+                    self?.activityIndicator.stopAnimating()
+                    self?.activityIndicator.isHidden = true
+                    self?.posterImgView.image = UIImage(data: data as Data? ?? Data())
+                    self?.viewModel?.cache.setObject(data as AnyObject, forKey: path as AnyObject)
+                }
+            })
+        }
     }
-
+    
+    override func prepareForReuse() {
+        titleLabel.text = nil
+        releaseDateLabel.text = nil
+        overviewLabel.text = nil
+        posterImgView.image = nil
+        activityIndicator.isHidden = true
+    }
 }
---
+++ Movie/MovieListViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  MovieListViewModel.swift
+//  Movie
+//
+//  Created by Navneet on 4/25/18.
+//  Copyright © 2018 Navneet. All rights reserved.
+//
+
+import Foundation
+
+struct MovieListViewModel {
+ 
+    let movie: String
+    let results: Results?
+    
+    func lazyLoadPage(_ page: Int, completion: @escaping CompletionBlock) {
+        guard
+            let totalPages = results?.totalPages,
+            page <= totalPages,
+            let encodedQueryString = movie.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+            let url = URL(string: "http://api.themoviedb.org/3/search/movie?api_key=2696829a81b1b5827d515ff121700838&query=\(encodedQueryString)&page=\(page)")
+            else { return }
+        
+        let engine = NetworkEngine()
+        engine.fetch(url, completion: completion)
+    }
+}

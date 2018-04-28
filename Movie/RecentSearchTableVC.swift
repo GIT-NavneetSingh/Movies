d704289402071bb589b81759ad5dcda1c099df02
Navneet <navneet.s@emirates.com>
2018-04-28 16:20:56 +0400

    Added Recent VC

---
+++ Movie/RecentSearchTableVC.swift
@@ -0,0 +1,40 @@
+//
+//  RecentSearchTableVC.swift
+//  Movie
+//
+//  Created by Navneet on 4/26/18.
+//  Copyright © 2018 Navneet. All rights reserved.
+//
+
+import UIKit
+
+protocol RecentSearchProtocol {
+    func controller(_ controller: RecentSearchTableVC, didSelectItem item: String?)
+}
+
+class RecentSearchTableVC: UITableViewController {
+
+    var delegate: RecentSearchProtocol?
+    var items: [String]? = ["Batman", "Superman", "Iron Man", "Devil"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items?.count ?? 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier", for: indexPath)
+        // Configure the cell...
+        cell.textLabel?.text = items?[indexPath.row]
+        return cell
+    }
+
+    // MARK: - Table view delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        delegate?.controller(self, didSelectItem: items?[indexPath.row])
+    }
+}
